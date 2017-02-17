29acf16377abb6090bd25a106804f9f56b5cd16c
Emily Lee <eseitz@alum.mit.edu>
2017-02-16 19:20:20 -0800

    hover over faces to see a new face

--- js/party-people.js
+++ js/party-people.js
@@ -4,49 +4,51 @@ var bridesmaids = {
         name: 'Amalia Lee',
         title: 'MoH',//'Maid of honor',
         label: 'Medium sister',
-        img: 'amalia.jpeg',
+        img: 'amalia',
+        img2: 'amalia2',
         story: 'Amalia is Aliisa\'s medium-sister. Everyday, she is good at singing, drawing cute tiny beavers, and doing science. Years ago, Aliisa and Amalia signed a contract that says they can never be mad at each other for more than five seconds.'
     },
     annika: {
         name: 'Annika Lee',
         title: 'Bridesmaid',
         label: 'Big sister',
-        img: 'annika.jpeg',
+        img: 'annika',
         story: 'Annika is Aliisa\'s little big sister. She is amazing at drawing, speaking Japanese, and having information about everything. Annika is in charge of sending Aliisa art inspiration, interesting news, and pictures of other people\'s dogs.'
     },
     avelina: {
         name: 'Avelina Lee',
         title: 'Bridesmaid',
         label: 'Little sister',
-        img: 'avi.jpeg',
+        img: 'avi',
+        img2: 'avi-yupi',
         story: 'Avelina (or Avi) is Aliisa\'s littlest sister. She is a world-class baker, hair-expert, linguist, and baby boss. Avi\'s love for Aliisa -- expressed by giving her food and cuddles -- is only surpassed by Avi\'s love for her cat, Yupi.'
     },
     emily: {
         name: 'Emily Lee',
         title: 'Bridesmaid',
         label: 'Sister-in-law, the first',
-        img: 'emily.JPG',
+        img: 'emily',
         story: 'Aliisa got a new sister when Emily married Anders in 2015! Emily is a coder, photographer, and perfect blend of creativity and organization. Emily is also a fun lifeline of sanity and Pinterest ideas when Aliisa desperately texts her for life help.'
     },
     alissa: {
         name: 'Alissa Hopper',
         title: 'Bridesmaid',
         label: 'Sister-in-love',
-        img: 'alissa.jpg',
+        img: 'alissa',
         story: 'Alissa is the sister Aliisa found at college! Alissa is a beautiful singer, a dog-whisperer, baby-whisperer. Aliisa and Alissa have oddly similar mannerisms and names, and we are bonded for life because of it.'
     },
     miriam: {
         name: 'Miriam Black',
         title: 'Bridesmaid',
         label: 'Sister-in-law-to-be',
-        img: 'miriam.jpg',
+        img: 'miriam',
         story: 'Miriam is basically Aliisa’s sister already. She Neuroscience, mice brains, crafts, Ellie. Mir-mom'
     },
     naomi: {
         name: 'Naomi Boyce',
         title: 'Bridesmaid',
         label: 'Sister-in-law-to-be',
-        img: 'naomi.jpg',
+        img: 'naomi',
         story: 'Naomi social worker, sporty, James,'
     }
 };
@@ -56,56 +58,57 @@ var groomsmen = {
         name: 'Daniel Hwang',
         title: 'Best man',
         label: 'Basically a brother',
-        img: 'daniel.jpeg',
+        img: 'daniel',
         story: '...'
     },
     andrew: {
         name: 'Andrew Black',
         title: 'Groomsman',
         label: 'Brother-in-law',
-        img: 'andrew.jpg',
+        img: 'andrew',
         story: '...'
     },
     billy: {
         name: 'Billy Boyce',
         title: 'Groomsman',
         label: 'Brother-in-law',
-        img: 'billy.jpeg',
+        img: 'billy',
         story: '...'
     },
     robby: {
         name: 'Robby Siliciano',
         title: 'Groomsman',
         label: 'Friend',
-        img: 'robby.JPG',
+        img: 'robby',
         story: '...'
     },
     josh: {
         name: 'Josh Hamilton',
         title: 'Groomsman',
         label: 'Friend, fellow Josh',
-        img: 'josh.jpg',
+        img: 'ham',
+        img2: 'ham2',
         story: '...'
     },
     aleksi: {
         name: 'Aleksi Lee',
         title: 'Groomsman',
         label: 'Brother-in-law-to-be',
-        img: 'aleksi.JPG',
+        img: 'aleksi',
         story: '...'
     },
     anders: {
         name: 'Anders Lee',
         title: 'Groomsman',
         label: 'Brother-in-law-to-be',
-        img: 'anders.JPG',
+        img: 'anders',
         story: '...'
     }
 };
 
 var parties = {
-    bridesmaids: bridesmaids,
-    groomsmen: groomsmen
+    'bride-tribe': bridesmaids,
+    'groom-crew': groomsmen
 };
 for (var party in parties) {
     for (var name in parties[party]) {
@@ -117,13 +120,20 @@ for (var party in parties) {
         }).data('name', person.name)
           .data('label', person.label)
           .data('story', person.story);
+        
+        console.log(person);
 
         var $face = $('<div/>', {
             'class': 'img-wrapper'
         }).append($('<img/>', {
             'class': 'party-face',
-            'src': 'images/wedding-party/' + party + '/' + person.img
+            'src': 'images/wedding-party/' + party + '/' + person.img + '.jpg'
         }));
+        
+        if (person.img2) {
+            $face.find('img').attr('src2', 'images/wedding-party/' + party + '/' + person.img2 + '.jpg');
+        }
+        
         $person.append($face);
 
         $person.append($('<p/>', {
@@ -145,10 +155,17 @@ for (var party in parties) {
 $('.party-rows .party-person').click(function () {
     $('.party-rows .party-person').removeClass('active');
     $(this).addClass('active');
-    $('#party-selected img')[0].src = $(this).find('img')[0].src;
+    $('#party-selected img').attr('src', $(this).find('img').attr('src'));
+    $('#party-selected img').attr('src2', $(this).find('img').attr('src2'));
     $('#party-selected .party-name').text($(this).data('name').toUpperCase());
     $('#party-selected .party-title').text($(this).data('label').toUpperCase());
     $('#party-selected .party-story').text($(this).data('story'));
+});
+
+$('#party-selected .party-face').hover(function () {
+    var newSrc = $(this).attr('src2');
+    $(this).attr('src2', $(this).attr('src'));
+    $(this).attr('src', newSrc);
 });
 
 // start with Amalia
